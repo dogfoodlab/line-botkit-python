007a26df6cb806b53706066e88fc13bd5d0441c5
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2021-07-22 19:33:03 +0900

    update

---
+++ tests/test_bot_i18n.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from line_botkit.bot_i18n import BotI18n
+
+
+def test_1(mocker):
+    handler_mock = mocker.MagicMock()
+    handler_mock.trans.return_value = 'test'
+
+    i18n = BotI18n(handler=handler_mock, language='ja')
+
+    assert i18n.trans('test') == 'test'
+
+    args, kwargs = handler_mock.trans.call_args
+
+    assert args[0] == 'test'
+    assert args[1] == 'ja_JP'
+
+
+def test_2(mocker):
+    handler_mock = mocker.MagicMock()
+    handler_mock.trans.return_value = 'test'
+
+    i18n = BotI18n(handler=handler_mock, language='unknown')
+
+    assert i18n.trans('test') == 'test'
+
+    args, kwargs = handler_mock.trans.call_args
+
+    assert args[0] == 'test'
+    assert args[1] == '__'
